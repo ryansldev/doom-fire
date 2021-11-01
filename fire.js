7a583c8e6125e2834b8bb0d049a7ab08483a941d
unknown <ryanlima1309@gmail.com>
2021-11-01 01:10:41 -0300

    :heart_on_fire: Starting!

---
+++ fire.js
@@ -0,0 +1,39 @@
+const firePixelsArray = [];
+const fireWidth = 10;
+const fireHeight = 10;
+
+function start() {
+  createFirePropagation();
+  renderFire();
+}
+
+function createFirePropagation() {
+  const numberOfPixels = fireWidth * fireHeight;
+
+  for(let i = 0; i < numberOfPixels; i++) {
+    firePixelsArray[i] = 0;
+  }
+}
+
+function renderFire() {
+  let html = '<table cellpadding=0 cellspacing=0>';
+
+  for(let row = 0; row < fireHeight; row++) {
+    html += '<tr>';
+
+    for(let column = 0; column < fireWidth; column++) {
+      const pixelIndex = column + ( fireWidth * row);
+      html += '<td>';
+      html += pixelIndex;
+      html += '</td>';
+    }
+
+    html += '</tr>';
+  }
+
+  html += '</table>';
+
+  document.querySelector('#fireCanvas').innerHTML = html;
+}
+
+start();
