56bba6cd3c8956b7808c135cc9a1c40396db5dbe
unknown <ryanlima1309@gmail.com>
2021-11-01 10:35:06 -0300

    :fire: Bettering the Render Fire function²

--- fire.js
+++ fire.js
@@ -43,7 +43,7 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
   const newFireIntensity =
     belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0;
 
-  firePixelsArray[currentPixelIndex] = newFireIntensity;
+  firePixelsArray[currentPixelIndex - decay] = newFireIntensity;
 }
 
 function renderFire() {
